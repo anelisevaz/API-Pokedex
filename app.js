0a3a426399b6c38ee91f097d8883958dd9b38947
anelisevaz.anelise <anevaz1@gmail.com>
2022-02-16 20:49:31 -0300

    Continuando

--- app.js
+++ app.js
@@ -12,11 +12,21 @@ const fetchPokemon = () => {
       const liPokemons = pokemons.reduce((accumulator, pokemon) => {
         const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
-        accumulator +='
-          <li class=card>
-          <img class="card-image ${types[0]}" alt="${pokemon.name} src=">
-        '
-      })
-    })
+        accumulator += `
+          <li class="card ${types[0]}">
+          <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+		  <h2 class="card-title">${pokemon.id}.${pokemon.name}</h2>
+		  <p class="card-subtitle">${types.join(' | ')}</p>
+		</li>
+`	
+	return accumulator
+},'')
+
+	const ul = document.querySelector('[data-js="pokedex"]')
+
+ ul.innerHTML = lisPokemons
+})
 
 }
+
+fetchPokemon()
