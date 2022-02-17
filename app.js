4f9c8151b0018c89b10ef0da73e3ee86e988e4f1
anelisevaz.anelise <anevaz1@gmail.com>
2022-02-16 21:55:55 -0300

    Corrigindo

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
 fetch(getPokemonUrl(index + 1)).then(response => response.json())) 
 
 const generateHTML = pokemons => pokemons.reduce((accumulator, {name, id, types}) => {
-	const elementsType = types.map(typeInfo => typeInfo.type.name)
+	const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
 accumulator += `
-	<li class="card ${elementsTypes[0]}">
+	<li class="card ${elementTypes[0]}">
 		<img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
 		<h2 class="card-title">${id}.${name}</h2>
-		<p class="card-subtitle">${elementsTypes.join(' | ')}</p>
+		<p class="card-subtitle">${elementTypes.join(' | ')}</p>
 	</li>
 `	
 	return accumulator
